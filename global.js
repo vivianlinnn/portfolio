e246b8069492577b317c42f53f6c257700a9193a
Vivian Lin <vivianlinnn260@gmail.com>
2025-01-23 11:50:56 -0800

    first part of step 3.1

--- global.js
+++ global.js
@@ -4,9 +4,19 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-let navLinks = $$("nav a");
-let currentLink = navLinks.find(
-    (a) => a.host === location.host && a.pathname === location.pathname
-  );
-  
-currentLink?.classList.add('current');
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'contact/', title: 'Contacts'},
+    { url: 'resume/', title: 'Resume'}
+  ];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+    nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+    // TODO create link and add it to nav
+}
