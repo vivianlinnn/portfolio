86c8ff7c06f54baaacb7e823281fa2535711939b
Vivian Lin <vivianlinnn260@gmail.com>
2025-01-23 17:12:40 -0800

    some switching

--- global.js
+++ global.js
@@ -19,15 +19,16 @@ const ARE_WE_HOME = document.documentElement.classList.contains('home');
 for (let p of pages) {
     let url = p.url;
     let title = p.title;
+    if (!ARE_WE_HOME && !url.startsWith('http')) {
+        url = '../' + url;
+    }
 
     let a = document.createElement('a');
     a.href = url;
     a.textContent = title;
     nav.append(a);
 
-    if (!ARE_WE_HOME && !url.startsWith('http')) {
-        url = '../' + url;
-    }
+    
 
 }
 
