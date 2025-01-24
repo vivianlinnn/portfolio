25980d2b224de4938ca8a3e7998312e75d15c55e
Vivian Lin <vivianlinnn260@gmail.com>
2025-01-23 17:37:32 -0800

    finished step 3.2

--- global.js
+++ global.js
@@ -28,8 +28,16 @@ for (let p of pages) {
     a.href = url;
     a.textContent = title;
     nav.append(a);
-
 }
+
+if (a.host === location.host && a.pathname === location.pathname) {
+    a.classList.add('current');
+}
+
+a.classList.toggle(
+    'current',
+    a.host === location.host && a.pathname === location.pathname
+);
 
 
 
