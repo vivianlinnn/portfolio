d1fe7492ceef6fb7c83a84d8264376c29f225e9f
Vivian Lin <vivianlinnn260@gmail.com>
2025-01-23 17:20:32 -0800

    hopefully working

--- global.js
+++ global.js
@@ -16,19 +16,19 @@ let nav = document.createElement('nav');
 document.body.prepend(nav);
 const ARE_WE_HOME = document.documentElement.classList.contains('home');
 
+
 for (let p of pages) {
     let url = p.url;
     let title = p.title;
+
     if (!ARE_WE_HOME && !url.startsWith('http')) {
         url = '../' + url;
     }
-
+    
     let a = document.createElement('a');
     a.href = url;
     a.textContent = title;
     nav.append(a);
-
-    
 
 }
 
