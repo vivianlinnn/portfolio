56c9130a843e24b12991cbbc7eb49bf17b1e55d7
Vivian Lin <vivianlinnn260@gmail.com>
2025-01-24 10:27:48 -0800

    changed

--- global.js
+++ global.js
@@ -38,6 +38,19 @@ for (let p of pages) {
     nav.append(a);
 }
 
+document.body.insertAdjacentHTML(
+    'afterbegin',
+    `
+      <label class="color-scheme">
+          Theme:
+          <select>
+              <option value="light dark">Automatic</option>
+              <option value="light">Light</option>
+              <option value="dark">Dark</option>
+          </select>
+      </label>`
+);
+
 
 
 
