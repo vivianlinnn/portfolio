1a56f6fff6bb451c3694b91f1cac1f7648346771
Vivian Lin <vivianlinnn260@gmail.com>
2025-01-23 16:49:22 -0800

    should work for all pages

--- global.js
+++ global.js
@@ -19,7 +19,11 @@ for (let p of pages) {
     let url = p.url;
     let title = p.title;
 
-    nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    nav.append(a);
+    
     const ARE_WE_HOME = document.documentElement.classList.contains('home');
     url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
 }
