a52d309a01daa0b59df99486e74a60aa7e6c74ea
Vivian Lin <vivianlinnn260@gmail.com>
2025-01-24 18:16:42 -0800

    4.4

--- global.js
+++ global.js
@@ -40,21 +40,25 @@ for (let p of pages) {
 
 document.body.insertAdjacentHTML(
     'afterbegin',
-    `
-      <label class="color-scheme">
+    `  <div class="color-scheme">
+      <label>
           Theme:
+          
           <select>
               <option value="light dark">Automatic</option>
               <option value="light">Light</option>
               <option value="dark">Dark</option>
           </select>
-      </label>`
+      </label>
+      </div>`
 );
-select = document.querySelector(select);
+const select = document.querySelector('.color-scheme');
+console.log(select)
 select.addEventListener('input', function (event) {
     console.log('color scheme changed to', event.target.value);
     document.documentElement.style.setProperty('color-scheme', event.target.value);
-  });
+    localStorage.colorScheme = event.target.value
+});
 
 
 
