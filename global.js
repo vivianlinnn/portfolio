5da92ab34f1eec713a639e07e772e5337e06e867
Vivian Lin <vivianlinnn260@gmail.com>
2025-01-30 12:53:51 -0800

    finished step 1.2

--- global.js
+++ global.js
@@ -69,3 +69,21 @@ select.addEventListener('input', function (event) {
     localStorage.colorScheme = event.target.value;
     
 });
+
+export async function fetchJSON(url) {
+    try {
+        // Fetch the JSON file from the given URL
+        const response = await fetch(url);
+        console.log(response)
+
+        if (!response.ok) {
+            throw new Error(`Failed to fetch projects: ${response.statusText}`);
+        }
+        const data = await response.json();
+        return data; 
+
+
+    } catch (error) {
+        console.error('Error fetching or parsing JSON data:', error);
+    }
+}
