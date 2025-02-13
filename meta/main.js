b1c0a4dbe4d8889c3088293fa964cdfca5894fe7
Vivian Lin <vivianlinnn260@gmail.com>
2025-02-12 20:10:39 -0800

    finished step 1

--- meta/main.js
+++ meta/main.js
@@ -11,9 +11,7 @@ async function loadData() {
       date: new Date(row.date + 'T00:00' + row.timezone),
       datetime: new Date(row.datetime),
     }));
-
-    processCommits();
-    console.log(commits);
+    displayStats();
   }
 
 document.addEventListener('DOMContentLoaded', async () => {
@@ -42,6 +40,9 @@ function processCommits() {
   
         Object.defineProperty(ret, 'lines', {
           value: lines,
+          writable: true,
+          enumerable: true,
+          configurable: true,
           // What other options do we need to set?
           // Hint: look up configurable, writable, and enumerable
         });
@@ -49,3 +50,80 @@ function processCommits() {
         return ret;
       });
   }
+
+function displayStats() {
+// Process commits first
+processCommits();
+// console.log(commits);
+
+// Create the dl element
+const dl = d3.select('#stats').append('dl').attr('class', 'stats');
+
+// Add total LOC
+dl.append('dt').html('Total <abbr title="Lines of code">LOC</abbr>');
+dl.append('dd').text(data.length);
+
+// Add total commits
+dl.append('dt').text('Total commits');
+dl.append('dd').text(commits.length);
+
+// Add max length
+
+let max_depth = d3.max(data, d => d.length)
+dl.append('dt').text('Max Depth');
+dl.append('dd').text(max_depth);
+
+// Add average file length
+const fileLengths = d3.rollups(
+    data,
+    (v) => d3.max(v, (v) => v.line),
+    (d) => d.file
+  );
+
+const averageFileLength = Math.round(d3.mean(fileLengths, (d) => d[1]));
+
+dl.append('dt').text('Average File Length');
+dl.append('dd').text(averageFileLength);
+
+
+// Add the most common day
+
+const date_num_reference = {
+    0: 'Sunday',
+    1: 'Monday',
+    2: 'Tuesday',
+    3: 'Wednesday',
+    4: 'Thrusday',
+    5: 'Friday',
+    6: 'Saturday',
+}
+
+const workbyDay = d3.rollups(
+    data,
+    (v) => v.length,
+    (d) => new Date(d.datetime).getDay());
+
+const maxDay = d3.greatest(workbyDay, (d) => d[1])?.[0];
+
+dl.append('dt').text('Day of the Week');
+dl.append('dd').text(date_num_reference[maxDay]);
+
+
+// get most productive time of day
+const workByPeriod = d3.rollups(
+    data,
+    (v) => v.length,
+    (d) => new Date(d.datetime).toLocaleString('en', { dayPeriod: 'short' })
+  );
+
+// console.log(workByPeriod);
+const maxPeriod = d3.greatest(workByPeriod, (d) => d[1])?.[0];
+
+dl.append('dt').text('Best Time of Day');
+dl.append('dd').text(maxPeriod);
+
+console.log(data);
+
+}
+
+
