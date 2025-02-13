3adf9cfafc0558f6e23bd40321ad247a1b264ae7
Vivian Lin <vivianlinnn260@gmail.com>
2025-02-13 10:42:47 -0800

    finished step 2.1

--- meta/main.js
+++ meta/main.js
@@ -12,6 +12,7 @@ async function loadData() {
       datetime: new Date(row.datetime),
     }));
     displayStats();
+    createScatterplot();
   }
 
 document.addEventListener('DOMContentLoaded', async () => {
@@ -125,5 +126,37 @@ dl.append('dd').text(maxPeriod);
 console.log(data);
 
 }
+
+function createScatterplot() {
+    const width = 1000;
+    const height = 600;
+    
+    const svg = d3
+      .select('#chart')
+      .append('svg')
+      .attr('viewBox', `0 0 ${width} ${height}`)
+      .style('overflow', 'visible');
+    
+    const xScale = d3
+    .scaleTime()
+    .domain(d3.extent(commits, (d) => d.datetime))
+    .range([0, width])
+    .nice();
+    
+    const yScale = d3.scaleLinear().domain([0, 24]).range([height, 0]);
+    
+    const dots = svg.append('g').attr('class', 'dots');
+    
+    dots
+      .selectAll('circle')
+      .data(commits)
+      .join('circle')
+      .attr('cx', (d) => xScale(d.datetime))
+      .attr('cy', (d) => yScale(d.hourFrac))
+      .attr('r', 5)
+      .attr('fill', 'steelblue');
+}
+
+
 
 
