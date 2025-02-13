d824ad053c23b804cae6294954642045cbe820d5
Vivian Lin <vivianlinnn260@gmail.com>
2025-02-13 11:17:14 -0800

    finished step 2.2

--- meta/main.js
+++ meta/main.js
@@ -155,6 +155,42 @@ function createScatterplot() {
       .attr('cy', (d) => yScale(d.hourFrac))
       .attr('r', 5)
       .attr('fill', 'steelblue');
+
+
+    const margin = { top: 10, right: 10, bottom: 30, left: 20 };
+
+    const usableArea = {
+        top: margin.top,
+        right: width - margin.right,
+        bottom: height - margin.bottom,
+        left: margin.left,
+        width: width - margin.left - margin.right,
+        height: height - margin.top - margin.bottom,
+      };
+      
+      // Update scales with new ranges
+      xScale.range([usableArea.left, usableArea.right]);
+      yScale.range([usableArea.bottom, usableArea.top]);
+
+      // Create the axes
+    const xAxis = d3.axisBottom(xScale);
+    const yAxis = d3
+    .axisLeft(yScale)
+    .tickFormat((d) => String(d % 24).padStart(2, '0') + ':00');
+
+    // Add X axis
+    svg
+    .append('g')
+    .attr('transform', `translate(0, ${usableArea.bottom})`)
+    .call(xAxis);
+
+    // Add Y axis
+    svg
+    .append('g')
+    .attr('transform', `translate(${usableArea.left}, 0)`)
+    .call(yAxis);
+
+
 }
 
 
