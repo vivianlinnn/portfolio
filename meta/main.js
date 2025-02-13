a098b91fc700942baff206de29b30c56754bc507
Vivian Lin <vivianlinnn260@gmail.com>
2025-02-12 17:33:15 -0800

    finished step 1.2

---
+++ meta/main.js
@@ -0,0 +1,51 @@
+let data = [];
+let commits = [];
+
+
+async function loadData() {
+    data = await d3.csv('loc.csv', (row) => ({
+      ...row,
+      line: Number(row.line), // or just +row.line
+      depth: Number(row.depth),
+      length: Number(row.length),
+      date: new Date(row.date + 'T00:00' + row.timezone),
+      datetime: new Date(row.datetime),
+    }));
+
+    processCommits();
+    console.log(commits);
+  }
+
+document.addEventListener('DOMContentLoaded', async () => {
+await loadData();
+});
+
+
+
+function processCommits() {
+    commits = d3
+      .groups(data, (d) => d.commit)
+      .map(([commit, lines]) => {
+        let first = lines[0];
+        let { author, date, time, timezone, datetime } = first;
+        let ret = {
+          id: commit,
+          url: 'https://github.com/vis-society/lab-7/commit/' + commit,
+          author,
+          date,
+          time,
+          timezone,
+          datetime,
+          hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
+          totalLines: lines.length,
+        };
+  
+        Object.defineProperty(ret, 'lines', {
+          value: lines,
+          // What other options do we need to set?
+          // Hint: look up configurable, writable, and enumerable
+        });
+  
+        return ret;
+      });
+  }
